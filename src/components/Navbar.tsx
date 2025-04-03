65bb08762d76b7c4ca23473c45d738788d5a4fae
Brian Okoth <brianoko2021@gmail.com>
2025-04-03 21:57:20 +0300

    Add icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
