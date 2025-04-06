b17877fea11a23a8b2eb93f6d7b5f66bfe4add79
Brian Okoth <brianoko2021@gmail.com>
2025-04-06 12:59:38 +0300

    Update footer contact email link for clarity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer} id='contact'>
-      <p>Contact us: <a href="mailto:brianoko2002@gmail.com">support@briotifx.com</a></p>
+      <p>Contact us: <a href="mailto:brianoko2002@gmail.com">support email</a></p>
       <p>© {new Date().getFullYear()} BRIOTIFX. All rights reserved.</p>
     </footer>
   );
